682234274aef95dd27401aec41c04ae8ada897d4
microStationCorp <sujanmondal916@gmail.com>
2020-03-29 18:35:21 +0530

    developer added

--- home/views.py
+++ home/views.py
@@ -4,7 +4,6 @@ from django.shortcuts import render, redirect
 
 
 def homePage(request):
-    print('home')
     if request.user.is_authenticated:
         return redirect('../user')
     return render(request, 'home/home.html')
