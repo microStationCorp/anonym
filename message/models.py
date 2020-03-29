da1e2bc710ed3770095705d8afdafcac46c861a9
microStationCorp <sujanmondal916@gmail.com>
2020-03-29 12:07:20 +0530

    new commmit

--- message/models.py
+++ message/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.utils import timezone
 # Create your models here.
+
+
+class RecievedMessage(models.Model):
+    reciever = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField()
+    dateOfPost = models.DateTimeField(default=timezone.now)
